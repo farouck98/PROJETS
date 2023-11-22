503b638886395cd332c6c616e01ef94dc8325b70
farouck98 <latoundjifarouck@gmail.com>
2023-11-22 15:38:11 +0100

    modification

--- chapitre_5/5.15.py
+++ chapitre_5/5.15.py
@@ -1,4 +1,4 @@
-chars_to_remove = ".,;:!?/'"
+
 lorem_ipsum = """Lorem Ipsum is simply dummy text of the printing and
 typesetting industry. Lorem Ipsum has been the industry's standard dummy
 text ever since the 1500s, when an unknown printer took a galley of type
@@ -8,13 +8,24 @@ essentially unchanged. It was popularised in the 1960s with the release of
 Letraset sheets containing Lorem Ipsum passages, and more recently with
 desktop publishing software like Aldus PageMaker including versions of
 Lorem Ipsum."""
-
+lorem_ipsum = lorem_ipsum.lower()
+chars_to_remove = ".,;:!?/'"
 
 for char in chars_to_remove:
-    lorem_ipsum = lorem_ipsum.replace(chars_to_remove, new:"" )
-print(lorem_ipsum)
+    lorem_ipsum = lorem_ipsum.replace(char," ")
+
+new_dic = {}
+list_word = lorem_ipsum.split()
+
+for word in list_word:
+    if word in new_dic:
+        new_dic[word] = new_dic[word] + 1
+    else:
+        new_dic[word] = 1
+
+print(new_dic)
 
 
 
 
-print(lorem_ipsum)
+
---
+++ chapitre_5/5.16.py
@@ -0,0 +1,10 @@
+l = [5, 1, 3, -2, 8]
+
+first_index =
+
+nl = []
+
+for i in l:
+    if i < l[0:4]:
+        nl.append(i)
+print(nl)
