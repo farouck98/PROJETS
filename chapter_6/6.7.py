e37c3a5f93bd7b6f31b57e49ef0593a53db8dec9
farouck98 <latoundjifarouck@gmail.com>
2023-11-24 10:46:38 +0100

    chapitre 6

--- chapter_6/6.7.py
+++ chapter_6/6.7.py
@@ -1,10 +1,13 @@
-e = ["quatre", "cinq", 4, 7]
+e = [1, 2, 3, 5, 9]
 """e2 = []
 for i in range(len(e)):
        e2.append(e[i-1])
 print(e2)"""
 
 def reverse_list(l: list):
+    if l == [] or l is not None:
        for i in range(len(l)):
             return [l[len(l) - i -1]for i in range(len(l))]
+    else:
+        return None
 print(reverse_list(e))
---
+++ chapter_6/6.8.py
@@ -0,0 +1,5 @@
+def reverse_number(nbr: float):
+    lnbr = [nbr]
+    for i in range(len(lnbr)):
+        
+print(reverse_number(123))
