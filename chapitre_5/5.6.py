9db25fa97c589d5f6809c6bab991b4c97c8852c9
farouck98 <latoundjifarouck@gmail.com>
2023-11-26 18:15:16 +0100

    chapitre_5

--- chapitre_5/5.6.py
+++ chapitre_5/5.6.py
@@ -1,10 +1,13 @@
-
 allowed_users = ["Alice", "Bob", "Charlie", "Michael"]
+allowed_users_lower = []
+username = input("Enter your username : ").lower()
 
-username = input("Entrez votre username : ")
-if username in allowed_users:
-    print("Vous êtes autorisé")
+for i in allowed_users:
+    allowed_users_lower.append(i.lower())
+if username in allowed_users_lower:
+    print("You are allowed")
 else:
-    print("Vous n'êtes pas autorisé")
+    print("You are not allowed : ")
+
 
 
